2112b7e8479a486b421cdfd2a7147ede9d0e1533
kian <kianizadpanah@gmail.com>
2026-03-02 23:20:14 +0200

    Add a script to load article files and its config

---
+++ configs/config.py
@@ -0,0 +1,23 @@
+import os
+
+DATASET_ROOT = os.environ.get("DATASET_ROOT", "./n")
+
+JSONS_DIR = os.path.join(DATASET_ROOT, "data", "jsons")
+
+SPLITS_DIR = os.path.join(DATASET_ROOT, "data", "splits")
+
+OUTPUT_DIR = os.environ.get("OUTPUT_DIR", "./outputs")
+
+BIAS_LABEL_MAP = {
+    0: "left",
+    1: "center",
+    2: "right",
+}
+
+BIAS_TEXT_MAP = {
+    "left": 0,
+    "center": 1,
+    "right": 2,
+}
+
+LOG_LEVEL = "INFO"
